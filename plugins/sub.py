a843996d0524bc62cd17e0eb71240e23cc8e1fbd
Mr-SyD <106866376+Mr-Syd@users.noreply.github.com>
2026-01-16 14:51:56 +0530

    Add join request handling for REQ_CHANNEL1 and REQ_CHANNEL2
    Handle join requests for specific channels and log errors.

--- plugins/sub.py
+++ plugins/sub.py
@@ -20,6 +20,19 @@ async def jreq_menu(_, message):
 
 
 
+@Client.on_chat_join_request(filters.chat(REQ_CHANNEL1) | filters.chat(REQ_CHANNEL2))
+async def join_reqs(b, join_req: ChatJoinRequest):
+    user_id = join_req.from_user.id
+    try:
+        if join_req.chat.id == REQ_CHANNEL1:
+            if join_req.invite_link.creator.id == b.me.id:
+                await db.add_req_one(user_id)
+        if join_req.chat.id == REQ_CHANNEL2:
+            if join_req.invite_link.creator.id == b.me.id:
+                await db.add_req_two(user_id)
+    except Exception as e:
+        print(f"Error adding join request: {e}")
+        
 @Client.on_callback_query(filters.regex("^jsyd:") & filters.user(ADMINS))
 async def jsyd_callback(client, cb):
     d = cb.data.split(":", 1)[1]
