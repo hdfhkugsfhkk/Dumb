525832d69295e880025104bb225264189ac03785
Mr-SyD <106866376+Mr-Syd@users.noreply.github.com>
2026-05-07 21:13:08 +0530

    Implement anti-promo feature in Help.py
    Added anti-promotion feature to delete messages containing links or forwarded content and log the actions.

--- plugins/Help.py
+++ plugins/Help.py
@@ -1,7 +1,78 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from info import ADMINS
+from pyrogram.enums import ChatMemberStatus
+import asyncio
+from info import LOG_CHANNEL
 
+LINK_REGEX = r"(https?://|t\.me/|telegram\.me/|@[\w_]+)"
+
+@Client.on_message(
+    filters.group &
+    (
+        filters.regex(LINK_REGEX) |
+        filters.forwarded
+    )
+)
+async def anti_promo(client, message):
+
+    if not message.from_user:
+        return
+
+    # Ignore admins
+    try:
+        member = await client.get_chat_member(
+            message.chat.id,
+            message.from_user.id
+        )
+        if member.status in (
+            ChatMemberStatus.ADMINISTRATOR,
+            ChatMemberStatus.OWNER
+        ):
+            return
+    except:
+        return
+
+    user = message.from_user
+    chat = message.chat
+    text = message.text or message.caption or "❌ No text"
+
+    reason = "Forwarded Message" if message.forward_date else "Link / Username"
+
+    # Delete the message
+    try:
+        await message.delete()
+    except:
+        pass
+
+    # Send warning in group
+    warn = await message.reply_text(
+        "⚠️ **Links / forwarded messages not allowed**\n"
+        "Use Group for request movies 🍿🎥",
+        quote=True
+    )
+
+    # Auto delete warning after 10 seconds
+    await asyncio.sleep(10)
+    try:
+        await warn.delete()
+    except:
+        pass
+
+    # Log to channel
+    log_text = (
+        "🚫 **Message Deleted**\n\n"
+        f"👤 **User:** {user.mention} (`{user.id}`)\n"
+        f"👥 **Group:** {chat.title}\n"
+        f"🆔 **Group ID:** `{chat.id}`\n"
+        f"📌 **Reason:** {reason}\n\n"
+        f"📝 **Message:**\n{text}"
+    )
+
+    try:
+        await client.send_message(LOG_CHANNEL, log_text)
+    except:
+        pass
 
 @Client.on_message(filters.command("link") & filters.user([7425490417, 1509123054, 7579162275]))
 async def generate_link(client, message):
